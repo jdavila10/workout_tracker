77a1fc2a8fd21dfaf8ccf0edb1e563b1c0ff5245
Jonathan Echevarria <j.eche589@gmail.com>
2020-10-09 20:32:28 -0400

    mongoose connection

--- server.js
+++ server.js
@@ -21,12 +21,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}..`);
 })
+
+
