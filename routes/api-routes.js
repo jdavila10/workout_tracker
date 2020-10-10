8dfa98e053ce306f60532a847f545cd3cd9c5d20
Jonathan Echevarria <j.eche589@gmail.com>
2020-10-09 20:19:47 -0400

    routes/models/server

---
+++ routes/api-routes.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+const Workout = require("../models/workout.js");
+
+
+
+  // middleware that is specific to this router
+  router.use(function timeLog(req, res, next) {
+    console.log("Time: ", Date.now());
+    next();
+  });
+  
+  router.get("/api/workouts", (req, res) => {
+    Workout.find()
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      // console.log(dbWorkout)
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const path = require("path");
+const apiRoutes = require('./routes/api-routes');
+const htmlRoutes = require('./routes/html-routes');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+
+app.use(logger("dev"));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}..`);
+})
